35ca2988bb125371dd9f79041669038edf5b7d67
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 11:52:10 -0500

    solución

--- ejercicio 3/ejercicio3.py
+++ ejercicio 3/ejercicio3.py
@@ -7,3 +7,39 @@
     de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y la cantidad 
     pendiente de cobro.
 """
+
+def main():
+    facturas = {}
+    acumulado = 0
+    cobrado = 0
+    bandera = True
+    while bandera:
+        print("-------------------------------OPCIONES--------------------------------------------")
+        print("1: Añadir nueva factura")
+        print("2: Pagar factura")
+        print("3: ¿Terminar?\n")
+        opcion = int(input(""))
+        if opcion == 1:
+            numero_factura = int(input("Ingresa el número de la factura: "))
+            valor_factura = float(input("Ingresa el valor de la factura: "))
+            facturas[numero_factura] = valor_factura
+            acumulado += valor_factura
+            print("Valor pendiente por pagar {}: ".format(acumulado))
+        elif opcion == 2:
+            numero_factura = int(input("Ingrese el número de la factura a pagar: "))
+            if numero_factura in facturas.keys():
+                cobrado += facturas[numero_factura]
+                acumulado -= facturas[numero_factura]
+                print("Valor de la factura {} \n".format(facturas[numero_factura]))
+                print("Cobrado hasta ahora {} queda pendiente un saldo de {}".format(cobrado, acumulado))
+                facturas.pop(numero_factura)
+                print("Factura cancelada con exito ")
+            else:
+                print("No se encontró la factura ")
+        elif opcion == 3:
+            bandera = False
+        else:
+            print("La opcion seleccionada no esta disponible. Intente de nuevo\n")
+            
+if __name__ == '__main__':
+    main()
