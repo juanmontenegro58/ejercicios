5b39e352a63d954138859166b1541939b04d2ef9
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:07:49 -0500

    enunciado

---
+++ ejercicio 9/ejercicio9.py
@@ -0,0 +1,10 @@
+""" 
+    El diccionario paises asocia cada persona con el conjunto de los países que ha visitado:
+    paises = {
+        'Pepito': {'Chile', 'Argentina'},
+        'Yayita': {'Francia', 'Suiza', 'Chile'},
+        'John': {'Chile', 'Italia', 'Francia', 'Peru'},
+    }
+    Escriba una funcion cuantos_en_comun(a, b), que indique cuántos países en común han visitado 
+    la persona a y la persona b:
+"""
