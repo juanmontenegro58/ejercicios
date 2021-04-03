9ccb8f7c3fb77c14a21aacd5fe45d599038ad312
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 11:50:12 -0500

    solución

--- ejercicio 2/ejercicio2.py
+++ ejercicio 2/ejercicio2.py
@@ -1,3 +1,10 @@
 """ 
     Escribir un programa que dada dos listas devuelva una nueva llsta con los valores intercalados.
 """
+def main(l1, l2):
+    new_list = []
+    for i, v in enumerate(l2):
+        new_list.append(l1[i])
+        new_list.append(v)
+    print(new_list)
+main(["Andres", "Carlos", "Alguien"],["Andrea", "Camila", "Johana"])
