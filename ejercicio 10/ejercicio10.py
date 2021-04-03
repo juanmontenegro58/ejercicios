8639147d472d3de8aac0009646dd52b2c90b20cf
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:11:07 -0500

    solución

--- ejercicio 10/ejercicio10.py
+++ ejercicio 10/ejercicio10.py
@@ -16,3 +16,59 @@
 
 """
 
+import time
+
+def decorador(funcion):
+    def medir_tiempo(*args, **kwargs):
+        inicio = time.time()
+        funcion(*args, **kwargs)
+        fin = time.time()
+        print("Ejecución de la funcion {} finalizada en {} \n".format(funcion.__name__,fin-inicio))
+    return medir_tiempo
+
+@decorador
+def total_por_alumno(tabla):
+    nueva_lista = []
+    for i in tabla:
+        asistencia = 0
+        for j in i:
+            if j:
+                asistencia += 1
+        nueva_lista.append(asistencia)
+    print(nueva_lista)
+
+@decorador
+def total_por_clase(tabla):
+    nueva_lista = []
+    for i in range(len(tabla[0])):
+        con = 0
+        for j in range(len(tabla)):
+            if tabla[j][i] == True:
+                con += 1
+        nueva_lista.append(con)
+    print(nueva_lista)
+    
+@decorador
+def alumno_estrella(alumnos,tabla):
+    alumno_estrella = ''
+    for k,i in enumerate(tabla):
+        asistencia = 0
+        for j in i:
+            if j:
+                asistencia += 1
+                if asistencia == len(i):
+                    alumno_estrella = alumnos[k]
+    if not alumno_estrella:
+        print("No hay alumno extrella. ")
+    print(alumno_estrella)
+
+alumnos = ['Pepito', 'Yayita', 'Fulanita', 'Panchito'] 
+asistencia = [
+        [True, True, True, False, False, False, False],
+        [True, True, True, False, True,  False, True ],
+        [True, True, True, True,  True,  True,  True ],
+        [True, True, True, False, True,  True,  True ]
+    ]
+total_por_alumno(asistencia)
+total_por_clase(asistencia)
+alumno_estrella(alumnos, asistencia)
