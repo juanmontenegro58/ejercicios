cc9a787eea688b93f5719abf12929f7dead9edf6
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 11:47:37 -0500

    enunciado

---
+++ ejercicio 1/ejercicio1.py
@@ -0,0 +1,5 @@
+""" 
+    Escribir un programa que almacene las asignaturas de un curso (Matemáticas, Física, Química, Historia y lengua)
+    en una lista, pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas
+    aprobadas. Al final el programa debe mostrar pon pantalla las asignaturas que el usuario tiene que repertir
+"""
