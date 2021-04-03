eb907ffae8c118a046ba38391d2e8461e96438f9
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:02:00 -0500

    enunciado

---
+++ ejercicio 7/ejercicio7.py
@@ -0,0 +1,6 @@
+""" 
+    Escriba la función contar_vocales(oracion) que retorne un diccionario asociando a cada vocal la cantidad de 
+    veces que aparece en la oracion. Si una vocal no aparece en la oración, de todos modos debe estar en el 
+    diccionario asociada al valor 0:
+"""
+
