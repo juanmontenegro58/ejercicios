28684b4f7c5d2e32ee69e77238b590eb8cba6162
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 11:51:47 -0500

    enunciado

---
+++ ejercicio 3/ejercicio3.py
@@ -0,0 +1,9 @@
+""" 
+    Escribir un programa que gestione las facturas pendientes de cobro de un empresa. Las facturas se almacenan
+    en un diccionario donde la clave de cada factura será el número de factura y el valor el coste de la factura.
+    El programa debe preguntar al usuario si quiere añadir una nueva factura, pagar una existente o terminar. Si
+    desea añadir una nueva factura se preguntará por el número de factura y su coste y se añadira al diccionario.
+    Si se desea pagar una factura se preguntará por el número de factura y se eliminara del diccionario. Después
+    de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y la cantidad 
+    pendiente de cobro.
+"""
