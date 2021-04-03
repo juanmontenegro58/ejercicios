b4f377ac9cb872ae9cff7dc094c9a11b00218f9d
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 11:53:59 -0500

    enunciado

---
+++ ejercicio 4/ejercicio4.py
@@ -0,0 +1,6 @@
+""" 
+    Escribir un programa para gestionar una agenda telefoníca con los nombres y los telefonos de los clientes de 
+    una empresa. El programa debera crear el fichero si no existe, para consultar el número de un cliente, añadir 
+    el telefono de un nuevo cliente y eliminar el número de un cliente. El nombre del cliente y su telefono deben
+    aparecer separados por comas y cada cliente en una linea distinta.
+"""
