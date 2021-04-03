60d2e70f80886af2ae71be4aac339cbe298f0168
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:05:43 -0500

    solución

--- ejercicio 8/ejercicio8.py
+++ ejercicio 8/ejercicio8.py
@@ -8,3 +8,27 @@
     Escriba un programa que cree un nuevo archivo llamado reporte.txt, en que cada línea indique
     si el alumno está aprobado (promedio ≥ 4,0) o reprobado (promedio < 4,0):
 """
+
+def main():
+    with open("notas.txt", "r") as file:
+        dic = {}
+        final = {}
+        data = file.readlines()
+        for i in data:
+            dic[i.split(":")[0]] = i.strip().split(":")[1:]
+
+        for k, v in dic.items():
+            total = 0
+            for i in v:
+                total += float(i)
+            final[k] = total / len(v)
+    with open("reporte.txt", "w") as file:
+        for k, v in final.items():
+            if v > 4.0:
+                file.write("{} Aprobado\n".format(k))
+            else:
+                file.write("{} Reprobado\n".format(k))
+    print("Guardado en reporte.txt")
+        
+if __name__ == '__main__':
+    main()
