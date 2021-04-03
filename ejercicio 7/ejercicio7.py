86cf023eb55aaa71a2bcc8fbb65900eddf3213b0
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:02:19 -0500

    solución

--- ejercicio 7/ejercicio7.py
+++ ejercicio 7/ejercicio7.py
@@ -4,3 +4,13 @@
     diccionario asociada al valor 0:
 """
 
+def contar_vocales(frase):
+    dic = {'a': 0, 'e': 0,'i':0,'o':0,'u':0}
+    for i in frase:
+        if i.lower() in dic.keys():
+            dic[i.lower()] += 1
+    print(dic)
+
+contar_vocales('Si no conocéis estos operadores es poco probable que vayáis a necesitarlos, por lo que podéis obviar esta\
+parte. Si aún así tenéis curiosidad os diré que estos son operadores que actúan sobre las representaciones\
+en binario de los operandos.')
