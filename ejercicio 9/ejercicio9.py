1a8b903c6fb3ee62b241b46d1b95a70883bb3048
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:08:32 -0500

    solución

--- ejercicio 9/ejercicio9.py
+++ ejercicio 9/ejercicio9.py
@@ -8,3 +8,15 @@
     Escriba una funcion cuantos_en_comun(a, b), que indique cuántos países en común han visitado 
     la persona a y la persona b:
 """
+
+def cuantos_en_comun(n1, n2):
+    paises = {
+        'Pepito': {'Chile', 'Argentina'},
+        'Yayita': {'Francia', 'Suiza', 'Chile'},
+        'John': {'Chile', 'Italia', 'Francia', 'Peru'},
+    }
+
+    comun = paises[n1].intersection(paises[n2])
+    print("Entre {} y {} la cantida de paises en común es {}".format(n1,n2, len(comun)))
+
+cuantos_en_comun("Pepito", "Yayita")
