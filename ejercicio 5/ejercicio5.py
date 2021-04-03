1d1be083785099761316d9ef32077065461e4318
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 11:57:16 -0500

    enunciado

---
+++ ejercicio 5/ejercicio5.py
@@ -0,0 +1,4 @@
+""" 
+    Escribir una función que reciba un diccionario con las asignaturas y las notas de un alumno y devuelva otro
+    diccionario con las asignaturas en mayusculas y las calificaciones en letras.
+"""
