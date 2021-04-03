00b60b8f324b2983385d8ac7ee6a3631e65b0c6c
juanmontenegro58 <juanmontenegro58@gmail.com>
2021-04-03 12:10:42 -0500

    enunciado

---
+++ ejercicio 10/ejercicio10.py
@@ -0,0 +1,18 @@
+""" 
+    La asistencia de los alumnos a clases puede ser llevada en una tabla, En un programa, esta informacion puede ser 
+    representada usando listas:
+    alumnos = ['Pepito', 'Yayita', 'Fulanita', 'Panchito']
+    asistencia = [
+        [True, True, True, False, False, False, False],
+        [True, True, True, False, True,  False, True ],
+        [True, True, True, True,  True,  True,  True ],
+        [True, True, True, False, True,  True,  True ]]
+    1. Escriba la función total_por_alumno(tabla) que reciba como parámetro la tabla de asistencia y retorne una lista 
+    con el número de clases a las que asistió cada alumno.
+    2. Escriba la función total_por_clase(tabla) que reciba como parámetro la tabla de asistencia y retorne una lista 
+    con el número de alumnos que asistió a cada clase.
+    3.Escriba la función alumno_estrella(asistencia) que indique qué alumno asistió más a clases.
+    4.Agregue un decorador que diga cuanto se tardo la ejecucion.
+
+"""
+
